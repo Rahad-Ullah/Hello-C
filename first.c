5b966465149f0847886691f5f46f5d14bf9acecf
Rahad-Ullah <rahadullah10@gmail.com>
2025-10-10 17:29:55 +0600

    learn: int, float, char data type

--- first.c
+++ first.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello World\n");
-    printf("Hello C\tProgramming");
+    int num1 = 10;
+    num1 = 20;
+    float num2 = 3.14;
+    char ch = 'a';
+    printf("%d \n", num1);
+    printf("%.2f \n", num2);  // %.2f means 2 decimal places
+    printf("%c", ch);
     return 0;
 }
