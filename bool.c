60782f98be6b6d00eb2c4ba6db3b1bed6aa9505e
Rahad-Ullah <rahadullah10@gmail.com>
2025-10-10 17:39:54 +0600

    learn: boolean data type

---
+++ bool.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<stdbool.h>  // this is for bool data type
+int main(){
+    bool a = true;
+    printf("%d", a);    // boolean doesn't have any format specifier. it will print 1 if true and 0 if false
+} 
